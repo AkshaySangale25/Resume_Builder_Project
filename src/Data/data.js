507a15f9a3e9df48309e52e6491d4016dfe822f0
Akshay Sangale <akshaysangale786@gmail.com>
2023-10-27 16:50:58 +0530

    add

---
+++ src/Data/data.js
@@ -0,0 +1,45 @@
+export const data = {
+
+  // ========== personal_info===========
+
+  personal_info: {
+    first_name: "Akshay",
+    last_name: "Subhash Sangale",
+    email: "sangaleakshay178@gmail.com",
+    mobile: "+919766990587",
+    address: "Karve Nagar, Hingne Home Colony, Pune",
+    city: "Pune",
+    state: "Maharashtra",
+    postal_code: "411052",
+    objective:
+      " Aspiring Full Stack Developer with a specialization in JavaScript, HTML, CSS, React, MangoDB, MySQL, Git/GitHub, NodeJS FetchApi, ExpressJS, DSA and design. Self-motivated and curious, with a keen interest in building user-centric products looking forward to honing my skill in a challenging work environment.",
+  },
+  // =============work_experience============
+
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Full Stack Developer",
+      organization_name: "AlmaBetter",
+      start_year: "2020",
+      end_year: "2021",
+    },
+    {
+      id: 2,
+      job_title: "Full Stack Developer",
+      organization_name: "AlmaBetter",
+      start_year: "2022",
+      end_year: "2023",
+    },
+  ],
+  // ==========education_details============
+  
+  education_details: {
+    type: "Graduation",
+    university: "Amaravati University",
+    degree: "Bachelor of Science",
+    start_year: "2016",
+    end_year: "2020",
+  },
+  key_skills: ["HTML", "CSS", "JavaScript", "ReactJS", "TailwindCss","MongoDB"],
+};
